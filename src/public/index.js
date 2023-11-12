f1362d9d160d209da02d38e4aac82a14059ba297
JuliCesar2022 <108636544+JuliCesar2022@users.noreply.github.com>
2023-11-12 17:09:01 -0500

    fix: corrige la imagen de la web

--- src/public/index.js
+++ src/public/index.js
@@ -6,6 +6,7 @@ const socket = io()
 const userId= "1";    
 const nombre = 'juancho' 
 const FirebaseToken= "ruytrueiwopqwoeirutyurieowpoeirutyurieowpeorituyg";
+const foto = 'https://d2v9ipibika81v.cloudfront.net/uploads/sites/104/2016/07/27885117831_d39c4bf5c8_k-1140x684.jpg'
 
 
 socket.emit('client:iniEmit:conection',{userId : userId, FirebaseToken , nombre})
@@ -16,7 +17,7 @@ socket.on('server:refresh:mensajes',(data)=>{
 
 function submit_message(){
     message = document.getElementById("message").value
-    socket.emit('client:send:message',{"mensaje": message,"nombre": "juana"})
+    socket.emit('client:send:message',{"mensaje": message,"nombre": "juana",foto:foto})
     document.getElementById("message").value = ''
 
 }

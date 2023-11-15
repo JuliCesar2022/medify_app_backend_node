56c2fe45cf58535adba233ee9a75943a403d3471
sergiovegam41 <59804620+sergiovegam41@users.noreply.github.com>
2023-11-15 13:24:19 -0500

    .

--- src/Jobs/ScheduledNotifications/ScheduledNotifications.js
+++ src/Jobs/ScheduledNotifications/ScheduledNotifications.js
@@ -1,49 +1,61 @@
 import NotificationsController from '../../Controllers/NotificationsController.js';
 import { DBNames } from './../../db.js';
+import moment from 'moment-timezone';
+
+
 // import axios from 'axios';
 
 
 class ScheduledNotifications {
 
-    static async run(MongoClient, hour){
+    static async run(MongoClient, utcHour){
 
-        // Obtén la fecha actual
+
         let fechaActual = new Date();
-
-        // Formatea la fecha al formato 'AAAA-MM-DD'
         let año = fechaActual.getFullYear();
-        let mes = fechaActual.getMonth() + 1; // getMonth() devuelve un índice basado en 0, así que suma 1
+        let mes = fechaActual.getMonth() + 1; 
         let dia = fechaActual.getDate();
 
-        // Asegúrate de que el día y el mes sean de dos dígitos
         mes = mes < 10 ? '0' + mes : mes;
         dia = dia < 10 ? '0' + dia : dia;
 
-        // Construye la cadena de fecha
         let fechaFormateada = año + '-' + mes + '-' + dia;
 
         let data = await MongoClient.collection(DBNames.medicamentos).find({
             iniciotratamiento: { $lte: fechaFormateada },
-            fintratameinto: { $gte: fechaFormateada } // Asegúrate de corregir la ortografía en tu base de datos.
+            fintratameinto: { $gte: fechaFormateada } 
           }).toArray();
 
-       
-
+       console.log(data)
+       data.forEach(medicamento => {
+           const frecuencia = parseInt(medicamento.frecuencia);
+           let hora = medicamento.horaInicial.split(':');
+           let horaProgramada = new Date();
+           horaProgramada.setHours(hora[0], hora[1], 0); 
+           
+           const horasMedicamento = []; 
+           
+           while(horaProgramada.getDate() === new Date().getDate()) { 
+            horasMedicamento.push(horaProgramada.toTimeString().substring(0, 5)); 
+            horaProgramada.setHours(horaProgramada.getHours() + frecuencia); 
+        }
         
-        //   console.log(tokens);
-        
 
-        // console.log(data);
+            let dateInColombia = moment.utc().hours(utcHour).minutes(0).seconds(0).tz('America/Bogota').format('HH');
 
-       
+            horasMedicamento.forEach(horaToma => {
 
+                if(dateInColombia == horaToma.split(":")[0]){
 
-
-     
                     
-      
+                    console.log("Notificacion de tima a "+horaToma)
+                }
+                
+            });
+          });
         
-
+        
+          
     }
     
  
